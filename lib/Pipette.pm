ba048f96d1aac4205dd5bad15c8709265355b112
Adam <adam@moonshado.com>
2013-02-21 05:40:39 -0500

    Adding function names.

--- lib/Pipette.pm
+++ lib/Pipette.pm
@@ -1,37 +1,245 @@
 package Pipette;
 
-use 5.00503;
-use strict;
-
 require Exporter;
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
 @ISA = qw(Exporter);
+@EXPORT = qw($_BASE $_CONFIG $_DESTROY_ENABLED $_PIPETTE_API_KEY 
+             $_PIPETTE_CLIENT_ID $_URI_ID $_URI_KEY);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Pipette ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-%EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-@EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-@EXPORT = qw(
-	
-);
-
+use vars qw($VERSION @ISA @EXPORT);
 $VERSION = '0.01';
 
+use strict;
 
-# Preloaded methods go here.
+use 5.010_000;
+
+BEGIN {
+    $_BASE = "";
+    $_CONFIG = "";
+    $_DESTROY_ENABLED = "";
+    $_PIPETTE_API_KEY = "";
+    $_PIPETTE_CLIENT_ID = "";
+    $_URI_ID = "";
+    $_URI_KEY = "";
+}
+
+
+###########
+## SETUP ##
+###########
+
+sub setup
+{
+
+}
+
+
+##############
+## API ROOT ##
+##############
+
+sub generateBaseQueryString
+{
+
+}
+
+
+sub getDocumentation
+{
+
+}
+
+
+sub displayDocumentation
+{
+
+}
+
+
+##############
+## DROPLETS ##
+##############
+
+sub showAllActiveDroplets
+{
+
+}
+
+
+sub showDroplet
+{
+
+}
+
+
+sub newDroplet
+{
+
+}
+
+
+sub rebootDroplet
+{
+
+}
+
+
+sub powerCycleDroplet
+{
+
+}
+
+
+sub shutdownDroplet
+{
+
+}
+
+
+sub powerOffDroplet
+{
+
+}
+
+
+sub powerOnDroplet
+{
+
+}
+
+
+sub resetRootPassword
+{
+
+}
+
+
+sub resizeDroplet
+{
+
+}
+
+
+sub takeSnapshot
+{
+
+}
+
+
+sub restoreDroplet
+{
+
+}
+
+
+sub rebuildDroplet
+{
+
+}
+
+
+sub enableAutomaticBackups
+{
+
+}
+
+
+sub disableAutomaticBackups
+{
+
+}
+
+
+sub destroyDroplet
+{
+
+}
+
+
+
+#############
+## REGIONS ##
+#############
+
+sub allRegions
+{
+
+}
+
+
+
+############
+## IMAGES ##
+############
+
+sub allImages
+{
+
+}
+
+
+sub showImage
+{
+
+}
+
+
+sub destroyImage
+{
+
+}
+
+
+
+##############
+## SSH KEYS ##
+##############
+
+sub allSSHKeys
+{
+
+}
+
+
+sub showSSHKey
+{
+
+}
+
+
+sub addSSHKey
+{
+
+}
+
+
+sub editSSHKey
+{
+
+}
+
+
+sub destroySSHKey
+{
+
+}
+
+
+
+###########
+## SIZES ##
+###########
+
+sub allSizes
+{
+
+}
+
+
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
+
 
 =head1 NAME
 
@@ -54,8 +262,6 @@ Blah blah blah.
 
 None by default.
 
-
-
 =head1 SEE ALSO
 
 Mention other useful documentation such as the documentation of
@@ -75,9 +281,5 @@ Adam M Dutko, E<lt>adutko@macosforge.orgE<gt>
 
 Copyright (C) 2013 by Adam M Dutko
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.12.4 or,
-at your option, any later version of Perl 5 you may have available.
-
-
-=cut
+This module may be modified, used, copied and redistributed at your own risk.
+Publicly redistributed modified versions must use a different name.
